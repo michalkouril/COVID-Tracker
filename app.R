4e83f1277911729125a84ee61190bd00425df7a0
PJ Van Camp <vancampn@mail.uc.edu>
2020-04-03 14:19:23 +0100

    Merge with manual simplifications before publishing new version
    Things removed are commented out

--- app.R
+++ app.R
@@ -9,6 +9,8 @@ if(!require(shinydashboard)) install.packages("shinydashboard", repos = "http://
 if(!require(shinythemes)) install.packages("shinythemes", repos = "http://cran.us.r-project.org")
 if(!require(scales)) install.packages("scales", repos = "http://cran.us.r-project.org")
 
+options(scipen=10000)
+Sys.setenv(TZ='America/New_York')
 
 ## Load in the data
 fipsData = read.csv("fipsData.csv", stringsAsFactors = F,  colClasses = "character") %>% 
@@ -40,45 +42,45 @@ ui <- navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
                sidebarLayout(  
                 sidebarPanel(
                   selectInput(inputId = "region",
-                              label = "Select one or more metro areas:",
+                              label = "Select one or more metro areas",
                               choices = sort(unique(fipsData$CSA.Title)),
-                              selected = c('Cincinnati-Wilmington-Maysville, OH-KY-IN',"Cleveland-Akron-Canton, OH","Columbus-Marion-Zanesville, OH"),
+                              selected = c("Seattle-Tacoma, WA","Boston-Worcester-Providence, MA-RI-NH-CT","San Jose-San Francisco-Oakland, CA"),
                               multiple = T, 
                               selectize = T,
                               width = "400px"
                   ),
                   helpText('Tip: type the city name for easy searching.'),hr(),
-                  radioButtons("outcome", "Outcome", list("Confirmed cases" = 1, "Deaths" = 2), inline = T),
-                  radioButtons("timeComp", "Time comparison", list("By date" = 1, "By numbers" = 2), inline = T),
-                  conditionalPanel(
-                    condition = "input.timeComp == 1 && input.advanced",
-                    dateRangeInput("dateSlider", "Date range", start = Sys.Date() - 21, end = Sys.Date())
-                  ),
-                  conditionalPanel(
-                    condition = "input.timeComp == 2 && input.advanced",
-                    numericInput("startCases", "Start number", min = 1, max = 10000, value = 10, step = 1)
-                    # numericInput("doublingRate", "Reference line days to double", min = 1, max = 21, value = 3, step = 1)
-                  ),
+                  radioButtons("outcome", "Data", list("Confirmed cases" = 1, "Deaths" = 2), inline = T),
+                  # radioButtons("yScale", "Time comparison", list("By date" = 1, "By numbers" = 2), inline = T),
+                  # conditionalPanel(
+                  #   condition = "input.yScale == 1 && input.advanced",
+                  #   dateRangeInput("dateSlider", "Date range", start = Sys.Date() - 21, end = Sys.Date())
+                  # ),
+                  # conditionalPanel(
+                  #   condition = "input.yScale == 2 && input.advanced",
+                  #   numericInput("startCases", "Start number", min = 1, max = 10000, value = 10, step = 1)
+                  #   numericInput("doublingRate", "Reference line days to double", min = 1, max = 21, value = 3, step = 1)
+                  # ),
                   radioButtons("yScale", "Scale", list("Linear" = 1, "Logarithmic" = 2), inline = T),
-                  checkboxInput("relPop", "Adjust for population size (per 10,000 people)"),
+                  conditionalPanel(
+                    condition = "input.yScale == 1",  
+                    radioButtons("relPop", "Adjust for population size", list("Yes" = 1, "No" = 2), inline = T, selected = 2)
+                  ),
                   tags$br(),
-                  tags$div(checkboxInput("advanced", "Show advanced options"), align = 'right', style = "color: gray;"),
+                  # tags$div(checkboxInput("advanced", "Show advanced options"), align = 'right', style = "color: gray;"),
                   tags$div(textOutput("filterWarnings"), style = "color: red;")
                 ),
                 mainPanel(
-                  plotOutput(outputId = 'plot1', width = "800px", height = "700px")
+                  plotOutput(outputId = 'plot1', width = "1000px", height = "600px")
                 )
               )
         ),
         tabPanel("About this site",
                  tags$div(
-                   tags$h4("Last data update"), 
-                   textOutput("updateTime"),
-                   HTML("<i>Updated once daily</i>"),
-                   tags$h4("Last app update"), 
-                   HTML("2020-04-01 16:54:36 EDT"), #Sys.time()
+                   tags$h4("Last update"), 
+                   textOutput("updateTime"), 'Data updated daily.',
                    tags$br(),tags$br(),tags$h4("Summary"),
-                   'This tool allows users to compare COVID-19 cases and growth rate across U.S. cities.',
+                   'This tool allows users to view COVID-19 cases and deaths across U.S. cities.',
                    tags$br(),tags$br(),tags$h4("Code"),
                    "Will release on GitHub soon.",
                    tags$br(),tags$br(),tags$h4("Sources"),
@@ -87,12 +89,12 @@ ui <- navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
                    tags$br(),tags$b("Population estimates: "), tags$a(href='https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html#par_textimage_70769902','The United States Census Bureau'), ".", 
                    tags$br(),tags$br(),'Inspiration for the design of these charts and this dashboard was derived from', tags$a(href='https://twitter.com/jburnmurdoch','John Burn-Murdoch'),' and', tags$a(href='https://github.com/eparker12/nCoV_tracker','Dr. Edward Parker'),', respectively.',
                    tags$br(),tags$br(),tags$h4("Authors"),
-                   'Benjamin Wissel, PJ Van Camp', tags$br(), 'Department of Biomedical Informatics',tags$br(),"Cincinnati Children's Hospital Medical Center",tags$br(),'University of Cincinnati College of Medicine',
+                   "Benjamin Wissel, Department of Biomedical Informatics, Cincinnati Children's Hospital Medical Center", tags$br(), "Dr. PJ Van Camp, Department of Biomedical Informatics, Cincinnati Children's Hospital Medical Center",
                    tags$br(),tags$br(),tags$h4("Contact"),
                    "benjamin.wissel@cchmc.org",tags$br(),
                    tags$a(href="https://twitter.com/BDWissel", "@bdwissel"), tags$br(), tags$br(),
                    "vancampn@mail.uc.edu",tags$br(),
-                   tags$a(href="https://www.linkedin.com/in/pjvancamp/", "LinkedIn"), tags$br(),
+                   tags$a(href="https://www.linkedin.com/in/pjvancamp/", "LinkedIn"),
                    tags$br(),tags$br(),tags$h4("Acknowledgements"),
                    'Thank you to', tags$a(href='https://www.cincinnatichildrens.org/bio/w/danny-wu','Dr. Danny Wu'), 'and ', tags$a(href='https://scholar.google.com/citations?user=NmQIjpAAAAAJ&hl=en','Sander Su'), 'for hosting this website on their server.',tags$br()
                  )
@@ -103,7 +105,7 @@ server <- function(input, output, session) {
   
   #USE THIS DURING TESTING
   covidData = reactive({
-    data = read.csv("us-counties3.31.csv", stringsAsFactors = F) %>% 
+    data = read.csv("us-counties.csv", stringsAsFactors = F) %>% 
       mutate(fips = as.character(fips), fips = ifelse(nchar(fips) < 5, paste0(0, fips), fips),
              date = as.Date(date)) %>% select(-county, - state)
     updateTime(as.character(max(data$date, na.rm = T)))
@@ -119,7 +121,7 @@ server <- function(input, output, session) {
   #   data
   # })
   
-  updateTime = reactiveVal(Sys.Date())
+  updateTime = reactiveVal('April 2, 12:30 PM EST.')
   filterWarning = reactiveVal("")
   
   #Update the time on the page
@@ -129,12 +131,13 @@ server <- function(input, output, session) {
   #Calculate the data to be displayed
    plot.data = reactive({
      
-     #If not advanced, set the startCases for cases to 10, deaths to 1
-     if(!input$advanced){
-       startCases = ifelse(input$outcome == 1, 10, 1)
-     } else {
-       startCases = input$startCases
-     }
+     startCases = ifelse(input$outcome == 1, 10, 1)
+     # #If not advanced, set the startCases for cases to 10, deaths to 1
+     # if(!input$advanced){
+     #   startCases = ifelse(input$outcome == 1, 10, 1)
+     # } else {
+     #   startCases = input$startCases
+     # }
      
      #Make sure the plot only gets generated if there is at least one region selected
      req(!is.null(input$region))
@@ -145,8 +148,8 @@ server <- function(input, output, session) {
      
      #If working by date, crop the data
      covidNumbers = covidData() 
-     if(input$timeComp == 1){
-       covidNumbers = covidNumbers%>% filter(between(date, input$dateSlider[1], input$dateSlider[2]))
+     if(input$yScale == 1){
+       covidNumbers = covidNumbers%>% filter(between(date, Sys.Date()-21, Sys.Date()))
      }
      
      #Build the data for the plot
@@ -167,7 +170,7 @@ server <- function(input, output, session) {
      
      #In case the plot needs to show starting from 10 cases
      omitted = NULL
-     if(input$timeComp == 2){
+     if(input$yScale == 2){
        plotData = plotData %>% 
          filter(y >= startCases) %>% group_by(region) %>% #Filter 10+
          mutate(x = 1:n() - 1) #Assign a number from 0 - n (days after first 10)
@@ -183,7 +186,7 @@ server <- function(input, output, session) {
      }
      
      #When cases per 10,000
-     if(input$relPop){
+     if(input$relPop == 1 && input$yScale == 1){
        plotData = plotData %>% 
          mutate(y = y / (population / 10000))
      }
@@ -195,22 +198,23 @@ server <- function(input, output, session) {
   #THis is the plot itself
   output$plot1 <- renderPlot({
     
-    #If not advanced, set the startCases for cases to 10, deaths to 1
-    if(!input$advanced){
-      startCases = ifelse(input$outcome == 1, 10, 1)
-    } else {
-      startCases = input$startCases
-    }
+    startCases = ifelse(input$outcome == 1, 10, 1)
+    # #If not advanced, set the startCases for cases to 10, deaths to 1
+    # if(!input$advanced){
+    #   startCases = ifelse(input$outcome == 1, 10, 1)
+    # } else {
+    #   startCases = input$startCases
+    # }
     
     plot = ggplot(plot.data(), aes(x=x, y=y, color = region)) +
-      geom_point() +
-      geom_line()
+      # geom_point() +
+      geom_line(size = 1.2)
     
     #Guide for doubling time
-    if(input$timeComp == 2 && !input$relPop){ #Only relevant when aligned by number of starting cases
+    if(input$yScale == 2){ #Only relevant when aligned by number of starting cases
       
       #If the population is relative, make sure the guide is too (is average population of ones shown)
-      pop = ifelse(input$relPop, 
+      pop = ifelse(F, 
                    mean(plot.data() %>% group_by(region) %>% summarise(p = max(population)) %>% pull(p)),
                    10000)
       
@@ -218,15 +222,15 @@ server <- function(input, output, session) {
       if(input$yScale == 2){
         plot = plot + 
           stat_function(fun = ~log10(doubleRate(.x, startCases, 2, pop)),
-                        linetype="dashed", colour = "#8D8B8B", size = 1.2, alpha = 0.3) +
+                        linetype="dashed", colour = "#8D8B8B", size = 1.0, alpha = 0.3) +
           stat_function(fun = ~log10(doubleRate(.x, startCases, 3, pop)),
-                        linetype="dashed", colour = "#8D8B8B", size = 1.2, alpha = 0.3)
+                        linetype="dashed", colour = "#8D8B8B", size = 1.0, alpha = 0.3)
       } else {
         plot = plot + 
           stat_function(fun = ~doubleRate(.x, startCases, 2, pop),
-                        linetype="dashed", colour = "#8D8B8B", size = 1.2, alpha = 0.3) +
+                        linetype="dashed", colour = "#8D8B8B", size = 1.0, alpha = 0.3) +
           stat_function(fun = ~doubleRate(.x, startCases, 3, pop),
-                      linetype="dashed", colour = "#8D8B8B", size = 1.2, alpha = 0.3)
+                      linetype="dashed", colour = "#8D8B8B", size = 1.0, alpha = 0.3)
       }
       
       plot = plot + ylim(c(NA, max(plot.data()$y))) 
@@ -240,27 +244,27 @@ server <- function(input, output, session) {
     
     #In case log-scale, add this to y-axis
     if(input$yScale == 2){
-      plot = plot + scale_y_log10()
+      plot = plot + scale_y_log10(labels = comma)
     } 
     
     #Finalize the plot
     #Labels depend on selections
-    xLabel = ifelse(input$timeComp == 1, "Date", 
-                    sprintf("Days from first %.0f or more %s", 
-                            startCases, 
-                            ifelse(input$outcome == 1, "cases", "deaths")))
-    yLabel = ifelse(input$relPop,ifelse(input$outcome == 1, "Cases per 10,000", "Deaths per 10,000"), 
-                    ifelse(input$outcome == 1, "Cases", "Deaths"))
+    xLabel = ifelse(input$yScale == 1, "Date", 
+                    paste("Number of Days Since",
+                          ifelse(input$outcome == 1, "10th Case", "1st Death")))
+    yLabel = ifelse(input$relPop == 1 && input$yScale == 1,ifelse(input$outcome == 1, "Cases per 10,000 Residents", "Deaths per 10,000 Residents"), 
+                    ifelse(input$outcome == 1, "Confirmed Cases", "Deaths"))
 
     plot + theme_bw() +
       labs(title = sprintf('COVID-19 %s in U.S. Metropolitan Areas', 
                            ifelse(input$outcome == 1, "Cases", "Deaths")),
-           caption =  ifelse(input$timeComp == 1 || input$relPop,
-                             "", "The dotted lines are the 2 and 3-day doubling rates ")) +
+           caption =  paste("Authors: Benjamin Wissel, PJ Van Camp\nData from The New York Times, ",
+                            "based on reports from state and local health agencies.\n",
+                            "http://bit.ly/covid-cities", sep = ""))  +
       xlab(xLabel) + ylab(yLabel) +
-      theme(legend.position = 'bottom', legend.direction = "vertical", 
+      theme(legend.position = 'right', legend.direction = "vertical", 
             legend.title = element_blank(), 
-            plot.caption = element_text(hjust = 0.5, vjust = 30, face = "italic", size = 15, colour = "#5c5d5e"), 
+            plot.caption = element_text(hjust = 0.0), 
             text = element_text(size=20))
     
   })
